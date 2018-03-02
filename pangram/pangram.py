0d57a2078946135c3ab601575a751f97dbf0bbf1
Zack Hartmann <zackhartmann@gmail.com>
2018-03-02 18:18:39 -0500

    Implemented Pangram Logic

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,12 @@
 def is_pangram(sentence):
-    pass
+	seen = set()
+	for x in sentence.lower():
+		if x.isalpha():
+			seen.add(x)
+
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	for c in alphabet:
+		if not c in seen:
+			return False
+
+	return True
