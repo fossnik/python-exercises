bcefd3ba5282fa50807b01ead1916f256257d169
Zack Hartmann <zackhartmann@gmail.com>
2018-03-02 18:04:03 -0500

    Implemented Case Insensitivity

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,10 +1,10 @@
 def is_isogram(string):
 	seen = set()
 	for x in string.lower():
-		if x in seen:
-			if x.isalpha():
+		if x.isalpha():
+			if x in seen:
 				return False
-		else:
-			seen.add(x)
+			else:
+				seen.add(x)
 
 	return True
