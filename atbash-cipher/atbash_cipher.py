54c034f4889280e91b44f0134b4430e4159c0b24
Zack Hartmann <zackhartmann@gmail.com>
2018-03-23 10:03:03 -0400

    Implemented Encode Logic

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,49 @@
-def encode(plain_text):
-    pass
+import re
 
+cipher = {
+	'a': 'z',
+	'b': 'y',
+	'c': 'x',
+	'd': 'w',
+	'e': 'v',
+	'f': 'u',
+	'g': 't',
+	'h': 's',
+	'i': 'r',
+	'j': 'q',
+	'k': 'p',
+	'l': 'o',
+	'm': 'n',
+	'n': 'm',
+	'o': 'l',
+	'p': 'k',
+	'q': 'j',
+	'r': 'i',
+	's': 'h',
+	't': 'g',
+	'u': 'f',
+	'v': 'e',
+	'w': 'd',
+	'x': 'c',
+	'y': 'b',
+	'z': 'a',
+}
+
+def encode(plain_text):
+	output = ""
+	count = 0
+	regexp = re.compile(r'[a-zA-Z0-9]')
+	for c in plain_text:
+		if regexp.search(c):
+			if count % 5 == 0 and count != 0:
+				output += " "
+			count += 1
+			if c.isalpha():
+				output += cipher[c.lower()]
+			elif c.isdigit():
+				output += c
+
+	return output
 
 def decode(ciphered_text):
-    pass
+	pass
