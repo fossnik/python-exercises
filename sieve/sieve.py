1cdebd3de5cfd59786014d897f1253e183de138e
Zack Hartmann <zackhartmann@gmail.com>
2018-03-26 11:46:59 -0400

    Implemented Solution

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,2 +1,17 @@
 def sieve(limit):
-    pass
+	if limit < 2:
+		return []
+
+	notPrime = []
+	for i in range(2, limit):
+		number = i
+		while number <= limit + i:
+			number += i
+			notPrime.append(number)
+
+	remit = [2]
+	for i in range(3, limit + 1):
+		if i not in notPrime:
+			remit.append(i)
+
+	return remit
