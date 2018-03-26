7a1f5098eeea93881e546522bfca29727172a16b
Zack Hartmann <zackhartmann@gmail.com>
2018-03-25 21:04:57 -0400

    Implemented Logic

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -1,2 +1,19 @@
 def largest_product(series, size):
-    pass
+	if not series:
+		raise Exception("ValueError")
+
+	if size > len(series):
+		raise Exception("ValueError")
+
+	max = 0
+
+	index = 0
+	while index <= len(series) - size:
+		temp = 1
+		for x in range(0, size):
+			temp *= int(series[index + x])
+		index += 1
+		if temp > max:
+			max = temp
+
+	return max
