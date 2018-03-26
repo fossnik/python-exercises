18bf63550b570840856a5e9623bbfc79f6763e0a
Zack Hartmann <zackhartmann@gmail.com>
2018-03-26 10:25:18 -0400

    Implement String and Array Logic

--- series/series.py
+++ series/series.py
@@ -1,2 +1,14 @@
 def slices(series, length):
-    pass
+	if length > len(series):
+		raise ValueError("slice too long")
+	if length < 1:
+		raise ValueError("slice too short")
+	remit = []
+	index = 0
+	while index <= len(series) - length:
+		subarray = []
+		for x in range(0, length):
+			subarray.append(int(series[index + x]))
+		remit.append(subarray)
+		index += 1
+	return remit
