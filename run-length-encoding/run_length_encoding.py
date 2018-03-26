53af189850524ddd6ad6d4bb7b9ec80a8c9f88e8
Zack Hartmann <zackhartmann@gmail.com>
2018-03-26 12:24:00 -0400

    Implemented Decode Logic

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,6 +1,18 @@
 def decode(string):
-    pass
-
+	remit = ""
+	x = 0
+	while x < len(string):
+		if not string[x].isdigit():
+			remit += string[x]
+		else:
+			prefix = ""
+			while string[x].isdigit():
+				prefix += string[x]
+				x += 1
+			for y in range(0, int(prefix)):
+				remit += string[x]
+		x += 1
+	return remit
 
 def encode(string):
-    pass
+	pass
