55321a2caf487843f52c275e432c91dc2747b9c0
Zack Hartmann <zackhartmann@gmail.com>
2018-03-26 09:59:26 -0400

    Implemented ASCII Logic

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,11 @@
+import re
 def rotate(text, key):
-
+	remit = ""
+	for c in text:
+		if re.match('[a-z]', c):
+			remit += chr(((ord(c) - ord('a')) + key) % 26 + ord('a'))
+		elif re.match('[A-Z]', c):
+			remit += chr(((ord(c) - ord('A')) + key) % 26 + ord('A'))
+		else:
+			remit += c
+	return remit
