4662cd85007409b8935a315a6f10d6240c7c02b7
Zack Hartmann <zackhartmann@gmail.com>
2018-03-30 11:56:11 -0400

    Implemented Logic

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,24 @@
 def hey(phrase):
-	pass
+	phrase = phrase.strip()
+
+	if isallcaps(phrase):
+		if phrase.endswith("?"):
+			return "Calm down, I know what I'm doing!"
+		else:
+			return "Whoa, chill out!"
+
+	if phrase.endswith("?"):
+		return "Sure."
+
+	if not phrase:
+		return "Fine. Be that way!"
+
+	return "Whatever."
+
+
+def isallcaps(phrase):
+	for x in phrase:
+		if x.islower():
+			return False
+
+	return True
