c2b0ade8fc2a33bbe416ab7480ec154f04be2b43
Zack Hartmann <zackhartmann@gmail.com>
2018-03-31 11:31:10 -0400

    Implemented Name Generation Logic

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,18 @@
+import random
+import string
+
+
+def namegenerate():
+	letters = ''.join(random.choice(string.ascii_uppercase) for _ in range(2))
+	digits = ''.join(random.choice(string.digits) for _ in range(3))
+	return letters + digits
+
+
 class Robot(object):
-    def __init__(self):
-        pass
+	name = ""
+
+	def __init__(self):
+		self.name = namegenerate()
+
+	def getname(self):
+		return self.name
